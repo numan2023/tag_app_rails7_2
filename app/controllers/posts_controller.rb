c023b6ac0a43b8f05be1a17e3146c2680b2a6326
numan2023 <fullmetal0212@gmail.com>
2023-11-12 23:58:41 +0900

    Initial commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,46 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post_form = PostForm.new
+  end
+
+  def create
+    @post_form = PostForm.new(post_form_params)
+    if @post_form.valid?
+      @post_form.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    post_attributes = @post.attributes
+    @post_form = PostForm.new(post_attributes)
+  end
+
+  def update
+    @post_form = PostForm.new(post_form_params)
+
+    if @post_form.valid?
+      @post_form.update(post_form_params, @post)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def post_form_params
+    params.require(:post_form).permit(:text, :image)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+end
